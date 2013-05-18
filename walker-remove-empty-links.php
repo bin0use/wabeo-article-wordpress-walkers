b1c265c2fa93bb9701d0bf64c357bf1b44e43571
Willy Bahuaud <w.bahuaud@gmail.com>
2013-05-18 19:30:17 +0200

    walker empty links init

---
+++ walker-remove-empty-links.php
@@ -0,0 +1,49 @@
+<?php
+function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+	$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+	$class_names = $value = '';
+
+	$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+	$classes[] = 'menu-item-' . $item->ID;
+
+	$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+	$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+	$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+	$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+	$output .= $indent . '<li' . $id . $value . $class_names .'>';
+
+	// MODIF 1
+    // remplacer SPAN par l'élément souhaité pour remplacer le  des liens vides
+    $balise = ( ! empty( $item->url ) && $item->url != '#') ? 'a' : 'span';
+
+	$atts = array();
+	$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+	$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+	$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+
+	// MODIF 2 : on n'ajout l'URL seulent si c'est un lien
+	if( 'a' == $balise )
+		$atts['href']   = ! empty( $item->url ) ? $item->url : '';
+
+	$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+	$attributes = '';
+	foreach ( $atts as $attr => $value ) {
+		if ( ! empty( $value ) ) {
+			$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+			$attributes .= ' ' . $attr . '="' . $value . '"';
+		}
+	}
+
+	// MODIF 3 : on remplace 'a' par $balise
+	$item_output = $args->before;
+	$item_output .= '<' . $balise . ''. $attributes .'>';
+	$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+	$item_output .= '</' . $balise . '>';
+	$item_output .= $args->after;
+
+	$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+}
