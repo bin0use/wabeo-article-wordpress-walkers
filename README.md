34bce4972f7bc06f69f25bbaf5e50c169ce07f86
Willy Bahuaud <w.bahuaud@gmail.com>
2013-05-18 18:12:23 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,10 @@
+WordPress et les JavaScripts asynchrones
+========
+
+## L'article sur wabeo
+
+Cet article à été publié sur [wabeo](http://wabeo.fr/blog/wordpress-javascripts-asynchrones/), les sources mis ici à disposition ont légèrement été revues et seront bientôt mises à jour. 
+
+## Le plugin
+
+Suite à cet article un plugin à été développé et est maintenant à disposition sur [l'extend de WordPress](http://wordpress.org/extend/plugins/wp-deferred-javascripts/)
---
+++ appel-walker.php
@@ -0,0 +1,19 @@
+wp_nav_menu(
+  array(
+    'theme_location'  => 'menu_primaire', // identifiant du menu, défini dans functions.php
+    'container'       => 'nav', // élément conteneur
+    'container_class' => 'class_menu_primaire', // classe de cet élément
+    'container_id'    => 'ID_menu_primaire', // ID de cet élément
+    'menu_class'      => 'class_du_menu', // class du menu
+    'menu_id'         => 'ID_du_menu', // ID du menu
+    'echo'            => true, //true si on veut écrire le menu, false pour un simple return
+    'fallback_cb'     => 'wp_page_menu', //fonction de substitution à utiliser si le menu n'existe pas
+    'before'          => '', // texte à mettre devant le lien
+    'after'           => '', // texte à mettre après le lien
+    'link_before'     => '', // texte par lequel commence le lien
+    'link_after'      => '', // texte par lequel termine le lien
+    'items_wrap'      => '<ul id="\&quot;%1$s\&quot;" class="\&quot;%2$s\&quot;">%3$s</ul>', //défini la forme du menu (ul, ol, rien...)
+    'depth'           => 0, // profondeur de menu admise (0 pour no-limit)
+    'walker'          => new My_Walker() // C'EST CET ÉLÉMENT QUI NOUS INTÉRESSE !!
+  )
+);
